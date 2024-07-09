cfe549281bfd80f9e4d77008af78d8ac5e4abea7
Ana Julia <59181829+anajurp@users.noreply.github.com>
2024-07-08 22:22:59 -0300

    Update read and process

--- src/automata.py
+++ src/automata.py
@@ -2,12 +2,13 @@
 
 def load_automata(filename):
     """Chama arquivo e abre."""
-    with open(filename, encoding="utf-8") as arq:
-        arquivo = arq.readlines()
-        linha = arquivo.split("\n")
+    try:
+        with open(filename, encoding="utf-8") as arq:
+            arquivo = arq.readlines()
+            linha = arquivo.split("\n")
+    except OSError:
+        print "OOpa, deu erro pra ler o arquivo, viu?"
 
-# Usado quando você usa a instrução "global" para atualizar uma variável global
-# Pylint desencoraja seu uso. Isso não significa que você não possa usá-lo!
 # pylint: disable=global-statement
         global ESTADOINICIAL
         ESTADOINICIAL = linha[0]
@@ -21,16 +22,33 @@ def load_automata(filename):
         NODOS = linha[4:]
 
 
+def procura_estado(automata, words):
+    """Procura estados."""
+    for nodo in NODOS:
+        n = nodo.split(" ")
+        if n[0] == automata and words == n[2]:
+            return n[1]
+    return None
+
+
 def process(automata, words):
-    """
-    Processa a lista de palavras e retora o resultado.
-    
-    Os resultados válidos são ACEITA, REJEITA, INVALIDA.
-    """
-
-    for word in words:
-        # tenta reconhecer `word`
-
+    # pylint: disable=unused-argument
+    """Processo."""
+    i = -1
+    for letra in words:
+        i += 1
+        atual = procura_estado(atual, letra) # noqa
+        if atual is None:
+            print("INVALIDA \n ")
+        else:
+            print(atual + ", " + letra)
+            if i != len(words) - 1:
+                print("Para")
+            else:
+                if atual in ESTADOSFINAIS:
+                    print("\n ACEITA ")
+                else:
+                    print("\n REJEITA")
 
 def convert_to_dfa(automata):
     """Converte um NFA num DFA."""
