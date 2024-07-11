5a72dc012ca412bde1ad3eb312804314f44a6e88
Ana Julia <59181829+anajurp@users.noreply.github.com>
2024-07-10 21:24:34 -0300

    Update automata.py 2

--- src/automata.py
+++ src/automata.py
@@ -1,3 +1,4 @@
+
 """Implementação de autômatos finitos."""
 
 def load_automata(filename):
@@ -50,5 +51,79 @@ def process(automata, words):
                 else:
                     print("\n REJEITA")
 
+def get_nextEstado_NFA(estado, letra, F):
+    statusTransicao = []
+    for regra in F:
+        if regra[1] == '&':
+            return "".join([regra[0],regra[2]])
+        if regra[0] == estado and regra[1] == letra and regra[2] not in statusTransicao:
+            statusTransicao.append(regra[2])
+        if regra[0] == estado and regra[1] == '&' and regra[2] not in statusTransicao:
+            statusTransicao.append(regra[2])
+
+    if statusTransicao == []:
+        return estado
+    else:
+        return "".join(statusTransicao)
+
 def convert_to_dfa(automata):
     """Converte um NFA num DFA."""
+
+    ESTADOINICIAL = automata[0]   
+    ALFABETO = automata[1] 
+    DELTA = automata[2] 
+    ESTADOFINAL = automata[3]   
+    NODOS = automata[4]   
+    
+    novo_estadoinicial = ESTADOINICIAL.copy()
+    novo_alfabeto = []
+    novo_estado = []
+    novo_estadofinal = ""
+    novo_nodo = []
+
+    novo_alfabeto.append(ESTADOFINAL)
+    for transicao in NODOS:
+        proximoEstado = get_nextEstado_NFA(transicao[0],transicao[1], NODOS)
+        if proximoEstado not in novo_alfabeto:
+            novo_alfabeto.append(proximoEstado)
+    novo_alfabeto = novo_alfabeto + ALFABETO
+
+    limpa_alfabeto = []
+    for estado_1 in novo_alfabeto:
+        for estado_2 in novo_alfabeto:
+            if estado_1 in estado_2 and estado_1 != estado_2:
+                limpa_alfabeto.append(estado_1)
+    
+    for deleta_estado in limpa_alfabeto:
+        novo_alfabeto.remove(deleta_estado)
+
+    for estado in DELTA:
+        for novo_estado in novo_alfabeto:
+            if estado in novo_estado:
+                novo_estado.append(novo_estado)
+
+    for novo_estado in novo_alfabeto:
+        if ESTADOFINAL in novo_estado:
+            novo_estadofinal = novo_estado
+            
+
+    for transicao in NODOS:
+        estado_inicial = transicao[0]
+        letra = transicao[1]
+        estado_final = transicao[2]
+        for novo_estado in novo_alfabeto:
+            if estado_inicial in novo_estado:
+                estado_inicial = novo_estado
+                break
+        for novo_estado in novo_alfabeto:
+            if estado_final in novo_estado:
+                estado_final = novo_estado
+        novo_nodo.append([estado_inicial, letra, estado_final])
+    
+    print(novo_estadoinicial)     
+    print(novo_alfabeto)
+    print(novo_estado)
+    print(novo_estadofinal)
+    print(novo_nodo)
+
+    return (novo_estadoinicial, novo_alfabeto, novo_estado, novo_estadofinal, novo_nodo)
